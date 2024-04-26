8b1e46ce2b4654c557b2dacc557d23b77f50038f
Azlam <43767972+azlam-abdulsalam@users.noreply.github.com>
2024-04-26 22:42:19 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/duckdbwasm-vitebrowser/',
     build: {
       target: "esnext" // Needed so that build can occur with the top-level 'await' statements
     }
